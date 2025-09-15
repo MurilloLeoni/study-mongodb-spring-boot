c975d640a08c253224591382d070d012f48c1bf0
Murillo Leoni <murilloleoni1999@gmail.com>
2025-09-15 13:23:56 -0300

    Project created

---
+++ src/main/java/com/murilloleoni/studieswithmongodb/StudieswithmongodbApplication.java
@@ -0,0 +1,13 @@
+package com.murilloleoni.studieswithmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudieswithmongodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudieswithmongodbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/murilloleoni/studieswithmongodb/StudieswithmongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.murilloleoni.studieswithmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudieswithmongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
