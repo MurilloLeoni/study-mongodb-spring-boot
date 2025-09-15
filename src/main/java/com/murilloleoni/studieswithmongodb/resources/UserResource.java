1e5de370508684040d81430220351b2fa2bb0df8
Murillo Leoni <murilloleoni1999@gmail.com>
2025-09-15 19:33:41 -0300

    put and delete implementation

--- src/main/java/com/murilloleoni/studieswithmongodb/resources/UserResource.java
+++ src/main/java/com/murilloleoni/studieswithmongodb/resources/UserResource.java
@@ -3,6 +3,7 @@ package com.murilloleoni.studieswithmongodb.resources;
 import com.murilloleoni.studieswithmongodb.domain.User;
 import com.murilloleoni.studieswithmongodb.dto.UserDTO;
 import com.murilloleoni.studieswithmongodb.services.UserService;
+import org.apache.coyote.http11.filters.VoidInputFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -38,6 +39,21 @@ public class UserResource {
         obj = service.insert(obj);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
         return ResponseEntity.created(uri).build();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id) {
+        User obj = service.fromDTO(objDto);
+        obj.setId(id);
+        obj = service.update(obj);
+        return ResponseEntity.noContent().build();
+    }
+
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Void> delete(@PathVariable String id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
