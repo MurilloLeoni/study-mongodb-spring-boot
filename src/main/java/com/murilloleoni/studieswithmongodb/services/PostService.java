d5b75f15596c01cf3558398307804ced9d33f90c
Murillo Leoni <murilloleoni1999@gmail.com>
2025-09-15 21:12:34 -0300

    Getting a post by id

---
+++ src/main/java/com/murilloleoni/studieswithmongodb/services/PostService.java
@@ -0,0 +1,20 @@
+package com.murilloleoni.studieswithmongodb.services;
+
+import com.murilloleoni.studieswithmongodb.domain.Post;
+import com.murilloleoni.studieswithmongodb.exception.ObjectNotFoundException;
+import com.murilloleoni.studieswithmongodb.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+//Conecta com o banco de dados, e utiliza funções prontas como findAll, findById...
+@Service
+public class PostService {
+
+    @Autowired //Próprio string instacia. Não precisa usar o 'new'
+    private PostRepository repo;
+
+    public Post findById(String id) {
+        return repo.findById(id)
+                .orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+}
